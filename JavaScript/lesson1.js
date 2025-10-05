c8f4dac47c672c5d3f74668da8891caf06b20bb0
hydralgorithm <hydralgorithm@gmail.com>
2025-10-05 11:21:19 +0530

    My old progress

---
+++ JavaScript/lesson1.js
@@ -0,0 +1,16 @@
+//This allows us to give basic output in the console whitespace (can find it using inspect element)
+console.log(`Hello`);
+console.log(`I like pizza`);
+//This gives an alert pop up while opening the page
+window.alert(`This is an alert!`);
+window.alert(`I like pizza!`);
+//We use it to print stuff in between h1 tags in html (note that the command written here are case-sensitive)
+document.getElementById("myh1").textContent=`Hello`;
+
+document.getElementById("myp").textContent=`I like Pizzaaa`
+// This is a comment
+/*This
+  is
+  a multi-line
+  comment
+*/
